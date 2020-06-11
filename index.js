870cd1da553b9769bfc498f196ab435e73f42993
Márcio Abrantes <abransoft@gmail.com>
2020-06-10 21:49:13 -0300

    Basics About RESTful API

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const Joi = require('joi');
+
 const app = express();
+app.use(express.json());
 
 const courses = [
     { id: 1, name: 'course1' },
@@ -7,29 +10,76 @@ const courses = [
     { id: 3, name: 'course3' }
 ];
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
-
 app.get('/api/courses', (req, res) => {
     res.send(courses);
 });
 
 app.get('/api/courses/:id', (req, res) => {
     const course = courses.find(c => c.id === parseInt(req.params.id));
-
     if (!course) {
-        res.status(404).send(`The course with the given ID=${req.params.id} was not found.`);
+        return res.status(404).send(`The course with the given ID=${req.params.id} was not found.`);
     }
 
     res.send(course);
-
 });
 
-app.get('/api/posts/:year/:month', (req, res) => {
-    res.send(req.params);
-    //res.send(req.query); // ?name=tal
+app.post('/api/courses', (req, res) => {
+    const { error } = validateCourse(req.body);
+    if (error) {
+        return res.send(400).send(error.details[0].message);
+    }
+
+    // Prepare
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    }
+
+    // Add
+    courses.push(course);
+    res.send(course);
 });
+
+app.put('/api/courses/:id', (req, res) => {
+    // Look up
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) {
+        return res.status(404).send(`The course with the given ID=${req.params.id} was not found.`);
+    }
+
+    // Validate
+    const { error } = validateCourse(req.body);
+    if (error) {
+        return res.status(400).send(error.details[0].message);
+    }
+
+    // Update
+    course.name = req.body.name;
+    res.send(course);
+});
+
+app.delete('/api/courses/:id', (req, res) => {
+    // Look up
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) {
+        return res.status(404).send(`The course with the given ID=${req.params.id} was not found.`);
+    }
+
+    // Delete
+    const index = courses.indexOf(course);
+    courses.splice(index, 1);
+
+    res.send(course);
+});
+
+
+function validateCourse(course) {
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+
+    return Joi.validate(course, schema);
+}
 
 
 const port = process.env.PORT || 3000;
@@ -37,3 +87,29 @@ app.listen(port, () => {
     console.log(`Listening on port ${port}...`);
 });
 
+
+
+
+
+
+
+
+
+
+
+
+
+// Manual validation
+/*if (!req.body.name || req.body.name.length < 3) {
+    // 400 Bad Request
+    res.status(400).send('Name is required and should be minimum 3 chars');
+    return;
+}*/
+
+// 404
+// Resource Not Found
+
+/*app.get('/api/posts/:year/:month', (req, res) => {
+    res.send(req.params);
+    //res.send(req.query); // ?name=tal
+});*/
